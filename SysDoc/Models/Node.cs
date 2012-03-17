b3a3be4c1acea9081d7c3aa84039d7752ee75311
JIrwin <jordan.s.irwin@gmail.com>
2012-03-17 14:01:42 -0700

    Adding basic Node

---
+++ SysDoc/Models/Node.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SysDoc.Models
+{
+    public class Node
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+    }
+}
